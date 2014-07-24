af3dc3a04ee39879c583c715a965a28cccc422ab
Tang Rufus <tangrufus@gmail.com>
2014-07-24 12:51:05 +0800

    Upgrade Sunny to Top Level Menu

--- admin/class-sunny-admin.php
+++ admin/class-sunny-admin.php
@@ -71,8 +71,8 @@ class Sunny_Admin {
 		$this->plugin_slug = $plugin->get_plugin_slug();
 
 		// Load admin style sheet and JavaScript.
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+		// add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
+		// add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
 
 		// Add the options page and menu item.
 		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
@@ -183,7 +183,8 @@ class Sunny_Admin {
 		 * - Change 'manage_options' to the capability you see fit
 		 *   For reference: http://codex.wordpress.org/Roles_and_Capabilities
 		 */
-		$this->plugin_screen_hook_suffix = add_options_page(
+
+		$this->plugin_screen_hook_suffix = add_menu_page(
 			__( 'Sunny Settings - Purge CloudFlare Cache', $this->plugin_slug ),
 			__( 'Sunny', $this->plugin_slug ),
 			'manage_options',
