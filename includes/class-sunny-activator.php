4da6a20e2fb57c2c630c8d004dedd46a7e0e6235
TangRufus <tangrufus@gmail.com>
2014-09-15 01:27:17 +0800

    Tag 1.4.7

--- includes/class-sunny-activator.php
+++ includes/class-sunny-activator.php
@@ -50,7 +50,7 @@ class Sunny_Activator {
 			self::enqueue_to_v142_admin_notice();
 		}
 
-		$current_version = '1.4.6';
+		$current_version = '1.4.7';
 		update_option( 'sunny_version', $current_version );
 
 	}
