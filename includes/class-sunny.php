1fe43bc7902ec7fab6bcb86e766eec55151cba9c
Tang Rufus <tangrufus@gmail.com>
2014-12-24 09:27:38 +0800

    Tagging 1.5.3

--- includes/class-sunny.php
+++ includes/class-sunny.php
@@ -56,7 +56,7 @@ class Sunny {
 	public function __construct() {
 
 		$this->plugin_name = 'sunny';
-		$this->version = '1.5.2';
+		$this->version = '1.5.3';
 
 		$this->load_dependencies();
 		$this->set_locale();
