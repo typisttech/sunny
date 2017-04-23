6f06dde97bd376a345bb45e0adfe91d51ffd49a8
Tang Rufus <tangrufus@gmail.com>
2017-04-23 19:08:10 +0800

    Add purge event

---
+++ src/Purge/Event.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Sunny
+ *
+ * Automatically purge CloudFlare cache, including cache everything rules.
+ *
+ * @package   Sunny
+ *
+ * @author    Typist Tech <sunny@typist.tech>
+ * @copyright 2017 Typist Tech
+ * @license   GPL-2.0+
+ *
+ * @see       https://www.typist.tech/projects/sunny
+ * @see       https://wordpress.org/plugins/sunny/
+ */
+
+declare(strict_types=1);
+
+namespace TypistTech\Sunny\Purge;
+
+/**
+ * Final class Event
+ */
+final class Event
+{
+    /**
+     * Reason to trigger a purge
+     *
+     * @var string
+     */
+    private $reason;
+
+    /**
+     * Urls to be purged
+     *
+     * @var string[]
+     */
+    private $urls;
+
+    /**
+     * Event constructor.
+     *
+     * @param string          $reason  Reason to trigger a purge.
+     * @param string|string[] ...$urls Urls to be purged.
+     */
+    public function __construct(string $reason, string ...$urls)
+    {
+        $this->reason = $reason;
+        $this->urls = $urls;
+    }
+
+    /**
+     * Reason getter.
+     *
+     * @return string
+     */
+    public function getReason(): string
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Urls getter.
+     *
+     * @return string[]
+     */
+    public function getUrls(): array
+    {
+        return $this->urls;
+    }
+}
