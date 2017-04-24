a5ad43de2e408f9f16ad69376fc55ca83c0f94a3
Tang Rufus <tangrufus@gmail.com>
2017-04-24 23:11:34 +0800

    codecept generate:suite restapi

---
+++ tests/_support/Helper/Restapi.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypistTech\Sunny\Helper;
+
+/**
+ * Here you can define custom actions
+ * All public methods declared in helper class will be available in $I
+ */
+class Restapi extends \Codeception\Module
+{
+}
